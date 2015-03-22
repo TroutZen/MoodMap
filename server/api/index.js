b1bf220556d2cc62c6fd9336c8f8e9e6bad00b98
TroutZen <devin.otway@gmail.com>
2015-03-21 22:35:14 -0700

    server boilerplate

---
+++ server/api/index.js
@@ -0,0 +1,9 @@
+var router = require('express').Router();
+
+module.exports = function(app){
+
+	require('./auth')(router);
+	require('./user')(router);
+	app.use('/api', router);
+
+};
--- server/app.js
+++ server/app.js
@@ -1,13 +1,55 @@
-var express = require('express');
-var path    = require('path');
+var app  = require('express')();
+var port = process.env.PORT || 3000;
 
-var port 		= process.env.PORT || 3000;
+// require in middleware
+require('./config.js')(app);
 
-// instantiate our express instance
-var app = express();
+// listen on port 
+app.listen(port, function(){
+	console.log('listening on port', port);
+});
+
+
+
+// var express 			= require('express'),
+// 		path    			= require('path'),
+//     expressJwt 		= require('express-jwt'),
+//     mongoose 			= require('mongoose'),
+//     morgan 				= require('morgan'),
+//     bodyParser 		= require('body-parser'),
+//     unless 				= require('express-unless'),
+//     NotFoundError = require(path.join(__dirname, "errors", "NotFoundError.js")),
+//     jwt 					= require('jsonwebtoken');
+
+// set the secret to sign and verify the jwt
+// var secret = "samadhi";
+
+
+// connect to db
+// var db = mongoose.connection;
+// db.on('open', function(){
+// 	console.log('connected to db');
+// });
+
+
+// middleware
+// app.use(bodyParser());
+
+
+
+
+
+// app.post('/login', function(req, res, next){
+// 	if (req.body.username === 'user')
+// });
+
+
+
+
+
 
 // serve from client/dist
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
 // app.use(require('MIDDLEWARE_PATH'));
 
 // you can require all your routes at once just point to directory
@@ -17,7 +59,3 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 // 	console.log('root route');
 // });
 
-// listen on port 
-app.listen(port, function(){
-	console.log('listening on port', port);
-});
