1dd58b85089c0be6eefb5a4a95c3cda8a926c6c5
TroutZen <devin.otway@gmail.com>
2015-03-22 01:50:36 -0700

    backend/add hash and salt to user model

--- server/models/user/index.js
+++ server/models/user/index.js
@@ -25,3 +25,39 @@ var UserSchema = new Schema({
 	symptoms: [String],
 	diagnoses: [String]
 });
+
+
+// salt/hash the pw
+UserSchema.pre('save', function(next){
+	var user = this;
+
+	// only hash the pw if it has been modified or is new
+	if (!user.isModified('password')) {
+		return next();
+	}
+
+	// generate salt
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+		if (err) {
+			return next(err);
+		}
+
+		// hash the pw along with the salt
+		bcrypt.hash(user.password, salt, function(err, hash){
+			if (err) return next(err);
+			// override the cleartext password with hashed pw
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+// compare supplied password with pw in db
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+
