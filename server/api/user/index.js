181e8340b1fb767ed2be2b047c5edb48bb1a7f05
TroutZen <devin.otway@gmail.com>
2015-03-22 21:54:52 -0700

    backend/add fetch and update for user

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,3 +1,22 @@
+var User = require('./userController.js');
+
 module.exports = function(router){
-	
+	// create user
+	// update user
+	// delete user
+	// read user
+
+	router.route('/users')
+		.post(User.create)
+		.get(User.list)
+
+	router.route('/users/:userId')
+		.put(User.update)
+		.delete(User.delete)
+		.get(User.fetch);
+
+
+
+
+
 };
--- server/api/user/userController.js
+++ server/api/user/userController.js
@@ -1,6 +1,77 @@
-var User     = require('../../models/user');
-var Promise  = require('bluebird');
-var mongoose = Promise.promisifyAll(require('mongoose'));
-// findOne User
+var User = require('../../models/user');
+
+// var UserSchema = new Schema({
+// 	username: {
+// 		type: String,
+// 		require: true,
+// 		unique: true,
+// 		lowercase: true
+// 	},
+
+// 	password: {
+// 		type: String,
+// 		required: true
+// 	},
+
+// 	firstname: String,
+// 	lastname: String,
+// 	age: Number,
+// 	dob: Date,
+// 	profile_pic: String,
+// 	symptoms: [String],
+// 	diagnoses: [String]
+// 	// TODO: Add nutrition with diet schema, supplement schema
+// 	// TODO: add fitness property
+// 	// TODO: add mind-body property
+// 	// TODO: add milestones/goals
+// 	// TODO: add bloodwork/health metrics
+// });
+
+var userController = {};
+
+// create new user
+userController.create = function(req, res, next){
+	var user = req.body;
+	User.create(user, function(err, user){
+		if (err) {
+			console.log('ERROR <userController> could not create user');
+			res.status(500);
+		}
+		else {
+			res.json(user);
+			console.log('created user', user);
+		}
+	});
+};
+
+// fetch user's data
+userController.fetch = function(req, res, next) {
+	var userId = req.params.userId;
+	User.findById(userId, function(err, user){
+		if (err) {
+			console.log('ERROR <userController> could not fetch user');
+			res.status(500);
+		} else {
+			res.json(user);
+			console.log('fetched user information', user);
+		}
+	});
+};
+
+userController.update = function(req, res, next) {
+	var userId = req.params.userId;
+
+	// changes is an object of properties to update
+	var changes = req.body;
+
+	User.findByIdAndUpdate(userId, changes, function(err, user){
+		if (err) {
+			console.log('ERROR <userController> could not update user');
+			res.status(500);			
+		}
+	});
+};
+
+module.exports = userController;
 
 
